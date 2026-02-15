58cfb83acf36f19496196c9c09ac1481c0dc4687
Maplewood <qlwy1991@gmail.com>
2026-02-15 17:49:16 +0800

    update __init__.py for new model code

--- model/__init__.py
+++ model/__init__.py
@@ -5,11 +5,15 @@ from model import AEDKWSASRPhone
 from model import AEDASR
 from model import AEDKWSASRPhoneUnet
 from model import TransformerKWSPhone
+from model import TransformerKWSPhone_hubert_wenet
+from model import TransformerKWSPhone_hubert_wenet_embed
 
 m_dict = {
     'AEDKWSASR': AEDKWSASR.AEDKWSASR,
     'AEDKWSASRPhone': AEDKWSASRPhone.AEDKWSASRPhone,
     'AEDASR': AEDASR.AEDASR,
     'AEDKWSASRPhoneUnet': AEDKWSASRPhoneUnet.AEDKWSASRPhoneUnet,
-    'TransformerKWSPhone': TransformerKWSPhone.TransformerKWSPhone
+    'TransformerKWSPhone': TransformerKWSPhone.TransformerKWSPhone,
+    'TransformerKWSPhone_hubert_wenet': TransformerKWSPhone_hubert_wenet.TransformerKWSPhone_hubert_wenet,
+    'TransformerKWSPhone_hubert_wenet_embed': TransformerKWSPhone_hubert_wenet_embed.TransformerKWSPhone_hubert_wenet_embed
 }
